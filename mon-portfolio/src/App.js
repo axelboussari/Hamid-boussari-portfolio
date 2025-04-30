de48462e0e6385435dce1a0b3210539eda5852be
axelboussari <axel.boussari@gmail.com>
2025-04-30 14:41:46 +0100

    [addition] add my picture on the home page of my portfolio

--- mon-portfolio/src/App.js
+++ mon-portfolio/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './Components/Header'
 import Home from './Components/Home'
+// import photo  from './Components/test.png'
 
 function App() {
   return (
     <div>
+    <title>Hamid's portfolio</title>
     <Header/>
     <Home/>
+    {/* <img src={photo}/> */}
     </div>
   );
 }
--- mon-portfolio/src/Components/Home.js
+++ mon-portfolio/src/Components/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Home.css'
-// import photo from '../../public/hamid.jpg'
+ import photo  from './images/hamid.png'
+
 
 
 export default function Home () {
     return (<div className="home">
         <div>
             <h1>Olawolé BOUSSARI</h1>
-            <h2 className='pres'>I am a <span class="typing"> Software engineer.</span></h2>
         </div>
         <div>
-            <img src="./hamid.png"/>
+            <h2 className='pres'>
+                I am a <span class="typing"> Software engineer.</span>
+            </h2>
         </div>
+            <img src={photo}/>
       </div>
-      )
+    )
   }
